e2af01f8eb82a4f79d44d464c054ba7f7d6f801d
Dreta <yangmain3@gmail.com>
2021-03-18 21:34:02 +0800

    Add copyright comment

--- lib/ui/home/account.dart
+++ lib/ui/home/account.dart
@@ -1,9 +1,27 @@
+/*
+ * Lilay is a custom Minecraft launcher.
+ * Copyright (c) 2021 Dreta
+ *
+ * Lilay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lilay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lilay.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 
 class Account extends StatelessWidget {
   final String name;
 
-  const Account({required this.name}) : assert(name != null);
+  const Account({required this.name});
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +30,18 @@ class Account extends StatelessWidget {
         tooltip: 'Account ' + name,
         offset: const Offset(50, 0),
         itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-              const PopupMenuItem(
-                  child: ListTile(
-                      leading: Icon(Icons.refresh),
-                      title: Text('Refresh'),
-                      minLeadingWidth: 20)),
-              const PopupMenuItem(
-                  child: ListTile(
-                      leading: Icon(Icons.delete, color: Colors.red),
-                      title:
-                          Text('Delete', style: TextStyle(color: Colors.red)),
-                      minLeadingWidth: 20))
-            ],
+          const PopupMenuItem(
+              child: ListTile(
+                  leading: Icon(Icons.refresh),
+                  title: Text('Refresh'),
+                  minLeadingWidth: 20)),
+          const PopupMenuItem(
+              child: ListTile(
+                  leading: Icon(Icons.delete, color: Colors.red),
+                  title:
+                  Text('Delete', style: TextStyle(color: Colors.red)),
+                  minLeadingWidth: 20))
+        ],
         child: ListTile(
             leading: Icon(Icons.account_circle, color: theme.accentColor),
             title: Text(name),
--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,3 +1,21 @@
+/*
+ * Lilay is a custom Minecraft launcher.
+ * Copyright (c) 2021 Dreta
+ *
+ * Lilay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lilay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lilay.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import 'package:lilay/ui/home/account.dart';
 import 'package:lilay/ui/home/profile.dart';
@@ -18,33 +36,33 @@ class _HomepageState extends State<Homepage> {
     return Row(children: [
       Drawer(
           child: ListView(padding: EdgeInsets.zero, children: <Widget>[
-        Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text('Lilay', style: textTheme.headline5)),
-        Divider(height: 1, thickness: 1, color: theme.dividerColor),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text('Lilay', style: textTheme.headline5)),
+            Divider(height: 1, thickness: 1, color: theme.dividerColor),
 
-        // Accounts
-        Padding(
-            padding:
+            // Accounts
+            Padding(
+                padding:
                 const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 5),
-            child: Text('ACCOUNTS', style: textTheme.subtitle2)),
-        Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: Divider(height: 1, thickness: 1, color: theme.dividerColor)),
-        Account(name: 'Dreta'),
-        Account(name: 'DretaAlt'),
+                child: Text('ACCOUNTS', style: textTheme.subtitle2)),
+            Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: Divider(height: 1, thickness: 1, color: theme.dividerColor)),
+            Account(name: 'Dreta'),
+            Account(name: 'DretaAlt'),
 
-        // Profiles
-        Padding(
-            padding:
+            // Profiles
+            Padding(
+                padding:
                 const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 5),
-            child: Text('PROFILES', style: textTheme.subtitle2)),
-        Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: Divider(height: 1, thickness: 1, color: theme.dividerColor)),
-        Profile(name: 'Fabric 1.16.5', modded: true),
-        Profile(name: 'Vanilla 1.16.5')
-      ]))
+                child: Text('PROFILES', style: textTheme.subtitle2)),
+            Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: Divider(height: 1, thickness: 1, color: theme.dividerColor)),
+            Profile(name: 'Fabric 1.16.5', modded: true),
+            Profile(name: 'Vanilla 1.16.5')
+          ]))
     ]);
   }
 }
