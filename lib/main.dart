790090b2166d511276fa47933d80e200a061ccf4
Dreta <yangmain3@gmail.com>
2021-03-18 21:28:52 +0800

    Add a logger (Core config setup isn't done yet)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lilay/ui/launcher.dart';
+import 'package:logging/logging.dart';
 
-void main() => runApp(Launcher());
+Logger logger = Logger('Lilay');
+
+void main() {
+  logger.onRecord.listen((record) {
+    print('[${record.level.name}] [${record.time}]: ${record.message}');
+  });
+
+  logger.info("Setting up core config...");
+
+  runApp(Launcher());
+}
