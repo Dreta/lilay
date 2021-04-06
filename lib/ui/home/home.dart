829a6eb0bdc93aa9b4d3596b184c60997b56c45d
Gabriel "Dreta" Leen <hello@dreta.dev>
2021-04-06 19:15:08 +0800

    Refactor

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -17,15 +17,13 @@
  */
 
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:lilay/core/auth/account.dart';
 import 'package:lilay/ui/accounts/accounts_provider.dart';
-import 'package:lilay/ui/accounts/accounts_section.dart';
 import 'package:lilay/ui/accounts/screen/accounts_screen.dart';
-import 'package:lilay/ui/home/profile.dart';
+import 'package:lilay/ui/home/navigation_drawer.dart';
 import 'package:lilay/ui/home/screen_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -33,12 +31,29 @@ import 'package:provider/provider.dart';
 /// consisting of the accounts, the game profiles
 /// and the play button.
 class Homepage extends StatelessWidget {
+  deleteAccount(BuildContext context, Account account) {
+    final ScreenProvider screen = Provider.of<ScreenProvider>(context);
+    final AccountsProvider accounts = Provider.of<AccountsProvider>(context);
+
+    if (accounts.accounts.length == 1) {
+      accounts.selectedAccount = null;
+      screen.current = Screen.home;
+    } else if (account.selected) {
+      account.selected = false;
+      for (Account account in accounts.accounts) {
+        if (account.uuid != account.uuid) {
+          accounts.selectedAccount = account;
+          break;
+        }
+      }
+    }
+    accounts.removeAccount(account.uuid);
+    accounts.saveTo(GetIt.I.get<File>(instanceName: 'accountsDB'));
+  }
+
   @override
   Widget build(BuildContext context) {
     final ScreenProvider screen = Provider.of<ScreenProvider>(context);
-    final AccountsProvider accounts = Provider.of<AccountsProvider>(context);
-    final ThemeData theme = Theme.of(context);
-    final TextTheme textTheme = theme.textTheme;
 
     return Scaffold(
         body: Container(
@@ -47,61 +62,12 @@ class Homepage extends StatelessWidget {
                     image: AssetImage('assets/background.png'),
                     fit: BoxFit.cover)),
             child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Theme(
-                  data: theme.copyWith(
-                      canvasColor: Colors.white.withOpacity(0.8)),
-                  child: ClipRRect(
-                      child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
-                          child: Drawer(
-                              child:
-                                  ListView(padding: EdgeInsets.zero, children: [
-                            Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child:
-                                    Text('Lilay', style: textTheme.headline5)),
-                            Divider(
-                                height: 1,
-                                thickness: 1,
-                                color: theme.dividerColor),
-
-                            // Accounts
-                            AccountsSection(),
-
-                            // Profiles
-                            Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 16, right: 16, top: 16, bottom: 5),
-                                child: Text('PROFILES',
-                                    style: textTheme.subtitle2)),
-                            Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 16, right: 16),
-                                child: Divider(
-                                    height: 1,
-                                    thickness: 1,
-                                    color: theme.dividerColor)),
-                            Profile(name: 'Fabric 1.16.5', modded: true),
-                            Profile(name: 'Vanilla 1.16.5')
-                          ]))))),
+              NavigationDrawer(),
               if (screen.current == Screen.accounts)
-                Expanded(child: AccountsScreen(onAccountDelete: (account) {
-                  if (accounts.accounts.length == 1) {
-                    accounts.selectedAccount = null;
-                    screen.current = Screen.home;
-                  } else if (account.selected) {
-                    account.selected = false;
-                    for (Account account in accounts.accounts) {
-                      if (account.uuid != account.uuid) {
-                        accounts.selectedAccount = account;
-                        break;
-                      }
-                    }
-                  }
-                  accounts.removeAccount(account.uuid);
-                  accounts
-                      .saveTo(GetIt.I.get<File>(instanceName: 'accountsDB'));
-                }))
+                Expanded(
+                    child: AccountsScreen(
+                        onAccountDelete: (account) =>
+                            deleteAccount(context, account)))
             ])));
   }
 }
---
+++ lib/ui/home/navigation_drawer.dart
@@ -0,0 +1,60 @@
+/*
+ * Lilay is a custom Minecraft launcher.
+ * Copyright (c) 2021 Dreta
+ *
+ * Lilay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lilay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lilay.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:lilay/ui/accounts/accounts_section.dart';
+import 'package:lilay/ui/home/profile.dart';
+
+/// This is the navigation drawer always shown in Lilay.
+class NavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextTheme textTheme = theme.textTheme;
+
+    return Theme(
+        data: theme.copyWith(canvasColor: Colors.white.withOpacity(0.8)),
+        child: ClipRRect(
+            child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
+                child: Drawer(
+                    child: ListView(padding: EdgeInsets.zero, children: [
+                  Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text('Lilay', style: textTheme.headline5)),
+                  Divider(height: 1, thickness: 1, color: theme.dividerColor),
+
+                  // Accounts
+                  AccountsSection(),
+
+                  // Profiles
+                  Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, top: 16, bottom: 5),
+                      child: Text('PROFILES', style: textTheme.subtitle2)),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: Divider(
+                          height: 1, thickness: 1, color: theme.dividerColor)),
+                  Profile(name: 'Fabric 1.16.5', modded: true),
+                  Profile(name: 'Vanilla 1.16.5')
+                ])))));
+  }
+}
