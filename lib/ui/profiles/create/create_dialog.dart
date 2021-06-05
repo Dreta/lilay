a23565d33d082968f05d2977e3b2a3b25bd00764
Gabriel "Dreta" Leen <hello@dreta.dev>
2021-06-05 15:45:16 +0800

    feat: automatically download the version manifest when creating a new profile

--- lib/ui/profiles/create/create_dialog.dart
+++ lib/ui/profiles/create/create_dialog.dart
@@ -16,27 +16,33 @@
  * along with Lilay.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker_cross/file_picker_cross.dart';
 import 'package:flutter/material.dart';
+import 'package:lilay/core/configuration/core/core_config.dart';
+import 'package:lilay/core/download/version/version_data.dart';
+import 'package:lilay/core/download/versions/latest_version.dart';
 import 'package:lilay/core/download/versions/version_info.dart';
 import 'package:lilay/core/download/versions/version_manifest.dart';
+import 'package:lilay/core/download/versions/versions_download_task.dart';
+import 'package:path/path.dart' as path;
+import 'package:provider/provider.dart';
 
 class CreateDialog extends StatefulWidget {
-  final VersionManifest versions;
-
-  CreateDialog({required this.versions});
+  CreateDialog();
 
   @override
-  _CreateDialogState createState() => _CreateDialogState(versions: versions);
+  _CreateDialogState createState() => _CreateDialogState();
 }
 
 class _CreateDialogState extends State<CreateDialog> {
   late String _selectedVersion = versions.latest.release;
-  final VersionManifest versions;
+  late final VersionManifest versions;
 
-  _CreateDialogState({required this.versions});
+  bool loaded = false;
+  double? progress = 0;
 
   final TextEditingController _name = TextEditingController();
   final FocusNode _versionFocus = FocusNode();
@@ -52,6 +58,77 @@ class _CreateDialogState extends State<CreateDialog> {
   final TextEditingController _javaExec = TextEditingController();
   final TextEditingController _jvmArgs = TextEditingController();
   final TextEditingController _gameArgs = TextEditingController();
+
+  /// Attempt to load the version manifest
+  void _loadVersions(BuildContext context) {
+    final CoreConfig config = Provider.of<CoreConfig>(context);
+
+    final VersionsDownloadTask task = VersionsDownloadTask(
+        progressCallback: (progress) =>
+            setState(() => this.progress = progress),
+        errorCallback: (error) {
+          // Construct our own version manifest from the downloaded versions
+          setState(() =>
+              progress = null); // Make the loading indicator indeterminate
+
+          // Enumerate the versions/ directory for valid versions
+          Directory versions = Directory(
+              '${config.workingDirectory}${Platform.pathSeparator}versions');
+          List<VersionInfo> versionObjs = [];
+          versions.list().listen((directory) async {
+            if (directory is Directory) {
+              File data = File(path.join(directory.absolute.path,
+                  '${path.basename(directory.path)}.json'));
+              File jar = File(path.join(directory.absolute.path,
+                  '${path.basename(directory.path)}.jar'));
+              if (await data.exists() && await jar.exists()) {
+                // If the game is executable (We check the hash later)
+                try {
+                  VersionData vData = VersionData.fromJson(jsonDecode(await data
+                      .readAsString())); // Parse and create the VersionInfo
+                  versionObjs.add(VersionInfo(
+                      vData.id, vData.type, '', vData.time, vData.releaseTime));
+                } catch (ignored) {
+                  // Ignore parsing errors for the version data - we will discard this version
+                }
+              }
+            }
+          });
+
+          // Sort and determine the latest version
+          versionObjs.sort((a, b) => (a.releaseTime.compareTo(b.releaseTime)));
+          String? latestRelease;
+          String? latestSnapshot;
+          for (VersionInfo version in versionObjs.reversed) {
+            if (version.type == VersionType.snapshot &&
+                latestSnapshot != null) {
+              latestSnapshot = version.id;
+              continue;
+            }
+            if (version.type == VersionType.release && latestRelease != null) {
+              latestRelease = version.id;
+              continue;
+            }
+          }
+          LatestVersion latest =
+              LatestVersion(latestRelease ?? '', latestSnapshot ?? '');
+
+          // Create the manifest
+          setState(() {
+            this.versions = VersionManifest(latest, versionObjs);
+            loaded = true;
+          });
+        },
+        resultCallback: (result) {
+          setState(() {
+            // Use the downloaded versions manifest
+            versions = result;
+            loaded = true;
+          });
+        },
+        workingDir: config.workingDirectory);
+    task.start(config.metaSource);
+  }
 
   /// Create the profile name field
   Widget _buildNameField(BuildContext context) {
@@ -274,16 +351,18 @@ class _CreateDialogState extends State<CreateDialog> {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Create a profile'),
-      content: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildNameField(context),
-            _buildVersionDropdown(context),
-            _buildGameDirectoryField(context),
-            _buildResolutionFields(context),
-            _buildAdvancedTile(context)
-          ]),
+      content: loaded
+          ? Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                  _buildNameField(context),
+                  _buildVersionDropdown(context),
+                  _buildGameDirectoryField(context),
+                  _buildResolutionFields(context),
+                  _buildAdvancedTile(context)
+                ])
+          : Center(child: CircularProgressIndicator(value: progress)),
       actions: [_buildSubmitButton(context)],
     );
   }
